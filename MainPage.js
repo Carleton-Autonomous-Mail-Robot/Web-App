0b1a2a9245b0416e2b500b9dba14bbe3f722caae
Devon Daley <devondaley@cmail.carleton.ca>
2021-02-04 22:56:01 -0500

    getting currentlocation every 1s, made use of mailbox

--- MainPage.js
+++ MainPage.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    
+    function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+	
     $("#requestButton").click(async function () {
         console.log("TEST!");
         
@@ -21,56 +24,72 @@ $(document).ready(function(){
 		// Encrypt jsondata
 		
 		
-		// Construct request
+		// Construct initial request
 		const request = new Request('http://localhost:5000', {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			body: jsondata});
 		
+		
 		// Send message, await response
 		let success = true;
 		let response = await fetch(request)
 			.catch((error) => {console.error('Error: ', error);success = false;});
-		let result = await response.json()
+		let jsonrec = await response.json()
 			.catch((error) => {console.error('Error: ', error);success = false;});
 			
-		// Decrypt result
+		// Decrypt jsonrec
 		
 		
 		// Only run this if webserver is listening and responding
 		if (success) {
+			// Construct json message, encrypt, and request structure for loop
+			var jsonloop = JSON.stringify({ 
+				"status": "good",
+				"clientID": jsonrec.payload.sender,
+				"opperation": "getMessage",
+				"payload": {
+					"sender": jsonrec.payload.sender,
+					"receiver": jsonrec.payload.receiver,
+					"pickup": source,
+					"destination": dest}});
+			//ENCRYPT TODO
+			const requestloop = new Request('http://localhost:5000', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: jsonloop});
+			
 			// Disable all inputs and button
 			document.getElementById('source').disabled = true;
 			document.getElementById('destination').disabled = true;
 			document.getElementById('requestButton').disabled = true;
-			
-			// Convert back to json
-			let jsonrec = JSON.parse(result);
 				
-			// Update clientID with the server's clientID
+			// Update clientID with the robot's clientID
 			document.getElementById('robotid').innerHTML = jsonrec.clientID;
 			
 			// Loop until robot has reached destination
-			var fetchNow = function() {
-			fetch('http://localhost:5000')
-				.then(function(response) {
-					// Decrypt response
-					
-					// Unpack payload from inside JSON
-					let jsonloc = JSON.parse(response.json());
-					
-					// Set innerHTML of currentlocation output box
-					document.getElementById('currentlocation').innerHTML = jsonloc.payload.currentLocation;
-					
-					// Loop if destination != current location
-					if(jsonloc.payload.currentLocation == jsonloc.payload.destination) {
-						;
-					}
-					else {
-						fetchNow();
-					}
-				})
-				.catch((error) => {console.error('Error: ', error);});
+			var fetchNow = async function() {
+				await sleep(1000);		// Get updates once per second to not overload system
+				fetch(requestloop)
+					.then(function(response) {
+						// Decrypt response
+						
+						
+						// Unpack payload from inside JSON
+						let jsonloc = response.json();
+						
+						// Set innerHTML of currentlocation output box
+						document.getElementById('currentlocation').innerHTML = jsonloc.payload.currentLocation;
+						
+						// Loop if destination != current location
+						if(jsonloc.payload.currentLocation == jsonloc.payload.destination) {
+							;
+						}
+						else {
+							fetchNow();
+						}
+					})
+					.catch((error) => {console.error('Error: ', error);});
 			};
 			
 			// Enable all inputs and button
