7a6cab441ff69c378423ac3af104b9004c4906eb
Devon Daley <devondaley@cmail.carleton.ca>
2021-02-06 01:00:58 -0500

    Error handling/display. Restructured, commented. Reduced coupling with server.

--- MainPage.js
+++ MainPage.js
@@ -4,85 +4,138 @@ $(document).ready(function(){
 	}
 	
     $("#requestButton").click(async function () {
-        console.log("TEST!");
         
-        // Fetch form info
-        let source = document.querySelector('#source');
-        let dest = document.querySelector('#destination');
-
-        // Insert into json, make string for transit. Server will fill blank sections
-        var jsondata = JSON.stringify({ 
-			"status": "good",
-			"clientID": "",
-			"opperation": "leaveMessage",
-			"payload": {
-				"sender": "",
-				"receiver": "",
-				"pickup": source,
-				"destination": dest}});
+        // FETCH FORM INFO AND RESET FIELDS
+			let source = document.querySelector('#source').value;
+			let dest = document.querySelector('#destination').value;
+			document.querySelector('#display').value = "";
+			document.querySelector('#robotid').value = "";
+			document.querySelector('#currentlocation').value = "";
         
-		// Encrypt jsondata
-		
-		
-		// Construct initial request
-		const request = new Request('http://localhost:5000', {
-			method: 'POST',
-			headers: {'Content-Type': 'application/json'},
-			body: jsondata});
-		
-		
-		// Send message, await response
-		let success = true;
-		let response = await fetch(request)
-			.catch((error) => {console.error('Error: ', error);success = false;});
-		let jsonrec = await response.json()
-			.catch((error) => {console.error('Error: ', error);success = false;});
+        // CREATE NEW CLIENT ON WEBSERVER, RESPONSE HAS ID INFO
+			var jsonnew = JSON.stringify({ 
+				"status": "good",
+				"clientID": "",
+				"opperation": "new",
+				"payload": {
+					"sender": "",
+					"receiver": "",
+					"pickup": source,
+					"destination": dest,
+					"currentLocation": ""}});
+					
+			// Todo encrypt
 			
-		// Decrypt jsonrec
-		
-		
-		// Only run this if webserver is listening and responding
-		if (success) {
-			// Construct json message, encrypt, and request structure for loop
+			let request = new Request('http://localhost:5000', {
+				method: 'POST',
+				mode: 'no-cors',
+				headers: {'Content-Type': 'application/json'},
+				body: jsonnew});
+				
+			// Send message, await response. Store IDs
+			let robotID = 0;
+			let clientID = 0;
+			let success = true;
+			let response = await fetch(request)		// Todo decrypt
+				.catch((error) => {console.error('Error: ', error);success = false;});
+			
+			if (success) {
+				let jsonrec = await response.json()	// Todo decrypt
+					.catch((error) => {console.error('Error: ', error);success = false;});
+				if (success) {
+					robotID = jsonrec.payload.receiver;
+					clientID = jsonrec.payload.sender;
+				} else {
+					document.querySelector('#display').value = "Invalid Response";
+					return "";
+				}
+				if (robotID == "") {
+					document.querySelector('#display').value = "No active robots";
+					return "";
+				}
+			} else {
+				document.querySelector('#display').value = "Connection failed, server not found";
+				return "";
+			}
+			
+        // IF YOU FOUND SERVER AND GOT VALID RESPONSE, MAKE OFFICIAL REQUEST
+			// Insert received data into json, make string for transit.
+			var jsonLmsg = JSON.stringify({ 
+				"status": "good",
+				"clientID": robotID,
+				"opperation": "leaveMessage",
+				"payload": {
+					"sender": clientID,
+					"receiver": robotID,
+					"pickup": source,
+					"destination": dest,
+					"currentLocation": ""}});
+				
+			// Todo encrypt
+				
+			request = new Request('http://localhost:5000', {
+				method: 'POST',
+				mode: 'no-cors',
+				headers: {'Content-Type': 'application/json'},
+				body: jsonLmsg});
+				
+			response = await fetch(request)		// Todo decrypt
+				.catch((error) => {console.error('Error: ', error);success = false;});
+				
+			if (success) {
+				let jsonrec = await response.json()	// Todo decrypt
+					.catch((error) => {console.error('Error: ', error);success = false;});
+				if (success == false) {
+					document.querySelector('#display').value = "Invalid Response JSON";
+					return "";
+				}
+			} else {
+				document.querySelector('#display').value = "Server not found";
+				return "";
+			}
+        
+        // DISABLE INPUTS UNTIL TASK FINISHED
+			document.querySelector('#source').disabled = true;
+			document.querySelector('#destination').disabled = true;
+			document.querySelector('#requestButton').disabled = true;
+			
+        // LOOP UNTIL currentLocation == destination
 			var jsonloop = JSON.stringify({ 
 				"status": "good",
-				"clientID": jsonrec.payload.sender,
+				"clientID": robotID,
 				"opperation": "getMessage",
 				"payload": {
-					"sender": jsonrec.payload.sender,
-					"receiver": jsonrec.payload.receiver,
+					"sender": clientID,
+					"receiver": robotID,
 					"pickup": source,
 					"destination": dest}});
-			//ENCRYPT TODO
-			const requestloop = new Request('http://localhost:5000', {
+					
+			// Todo encrypt
+			
+			request = new Request('http://localhost:5000', {
 				method: 'POST',
 				headers: {'Content-Type': 'application/json'},
 				body: jsonloop});
-			
-			// Disable all inputs and button
-			document.getElementById('source').disabled = true;
-			document.getElementById('destination').disabled = true;
-			document.getElementById('requestButton').disabled = true;
 				
-			// Update clientID with the robot's clientID
-			document.getElementById('robotid').innerHTML = jsonrec.clientID;
+			// Update display with the robotID
+			document.querySelector('#robotid').value = robotID;
 			
 			// Loop until robot has reached destination
+			let i = 0;
 			var fetchNow = async function() {
 				await sleep(1000);		// Get updates once per second to not overload system
-				fetch(requestloop)
+				fetch(request)
 					.then(function(response) {
-						// Decrypt response
-						
+						// Todo decrypt
 						
 						// Unpack payload from inside JSON
-						let jsonloc = response.json();
+						let jsonrec = response.json();
 						
 						// Set innerHTML of currentlocation output box
-						document.getElementById('currentlocation').innerHTML = jsonloc.payload.currentLocation;
+						document.querySelector('#currentlocation').value = jsonrec.payload.currentLocation;
 						
 						// Loop if destination != current location
-						if(jsonloc.payload.currentLocation == jsonloc.payload.destination) {
+						if(jsonrec.payload.currentLocation == jsonrec.payload.destination) {
 							;
 						}
 						else {
@@ -91,15 +144,17 @@ $(document).ready(function(){
 					})
 					.catch((error) => {console.error('Error: ', error);});
 			};
+        
+        // RESET ALL FIELDS, DELETE ASSOCIATED CLIENT
+			document.querySelector('#source').disabled = false;
+			document.querySelector('#destination').disabled = false;
+			document.querySelector('#requestButton').disabled = false;
 			
-			// Enable all inputs and button
-			document.getElementById('source').disabled = false;
-			document.getElementById('destination').disabled = false;
-			document.getElementById('requestButton').disabled = false;
+			document.querySelector('#currentlocation').value = "";
+			document.querySelector('#robotid').value = "";
+			document.querySelector('#display').value = "";
 			
-			// Clear relevant fields
-			document.getElementById('currentlocation').innerHTML = "";
-			document.getElementById('robotid').innerHTML = "";
-		}
+			
+			// Todo delete client
     });
 });
