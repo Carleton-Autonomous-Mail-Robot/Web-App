c073faa4d2b3f04d155ad3ffe147c565d3070c1c
Devon Daley <devondaley@cmail.carleton.ca>
2021-02-08 16:08:40 -0500

    fixed logic mistakes, delete client after completion

--- MainPage.js
+++ MainPage.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
     function sleep(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
@@ -11,12 +12,18 @@ $(document).ready(function(){
 			document.querySelector('#display').value = "";
 			document.querySelector('#robotid').value = "";
 			document.querySelector('#currentlocation').value = "";
+			
+		// ENSURE THAT DEST != SRC
+		if (dest == source) {
+			document.querySelector('#display').value = "Invaid because destination = source";
+			return "";
+		}
         
         // CREATE NEW CLIENT ON WEBSERVER, RESPONSE HAS ID INFO
 			var jsonnew = JSON.stringify({ 
 				"status": "good",
 				"clientID": "",
-				"opperation": "new",
+				"opperation": "newClient",
 				"payload": {
 					"sender": "",
 					"receiver": "",
@@ -40,18 +47,15 @@ $(document).ready(function(){
 				.catch((error) => {console.error('Error: ', error);success = false;});
 			
 			if (success) {
-				let jsonrec = await response.json()	// Todo decrypt
-					.catch((error) => {console.error('Error: ', error);success = false;});
-				if (success) {
+				let jsonrec = response.json();
+				// Todo decrypt
+				if (jsonrec.status == "done") {
 					robotID = jsonrec.payload.receiver;
 					clientID = jsonrec.payload.sender;
-				} else {
-					document.querySelector('#display').value = "Invalid Response";
-					return "";
-				}
-				if (robotID == "") {
-					document.querySelector('#display').value = "No active robots";
-					return "";
+					if (robotID == "") {
+						document.querySelector('#display').value = "No active robots";
+						return "";
+					}
 				}
 			} else {
 				document.querySelector('#display').value = "Connection failed, server not found";
@@ -83,12 +87,7 @@ $(document).ready(function(){
 				.catch((error) => {console.error('Error: ', error);success = false;});
 				
 			if (success) {
-				let jsonrec = await response.json()	// Todo decrypt
-					.catch((error) => {console.error('Error: ', error);success = false;});
-				if (success == false) {
-					document.querySelector('#display').value = "Invalid Response JSON";
-					return "";
-				}
+				jsonrec = response.json()	// Todo decrypt
 			} else {
 				document.querySelector('#display').value = "Server not found";
 				return "";
@@ -102,7 +101,7 @@ $(document).ready(function(){
         // LOOP UNTIL currentLocation == destination
 			var jsonloop = JSON.stringify({ 
 				"status": "good",
-				"clientID": robotID,
+				"clientID": clientID,
 				"opperation": "getMessage",
 				"payload": {
 					"sender": clientID,
@@ -121,29 +120,31 @@ $(document).ready(function(){
 			document.querySelector('#robotid').value = robotID;
 			
 			// Loop until robot has reached destination
-			let i = 0;
-			var fetchNow = async function() {
+			var fetchNow = await new Promise(resolve => async function() {
 				await sleep(1000);		// Get updates once per second to not overload system
 				fetch(request)
 					.then(function(response) {
 						// Todo decrypt
 						
 						// Unpack payload from inside JSON
-						let jsonrec = response.json();
+						jsonrec = response.json();
 						
-						// Set innerHTML of currentlocation output box
-						document.querySelector('#currentlocation').value = jsonrec.payload.currentLocation;
-						
-						// Loop if destination != current location
-						if(jsonrec.payload.currentLocation == jsonrec.payload.destination) {
-							;
-						}
-						else {
-							fetchNow();
+						// Check if the getmessage succeeded
+						if (jsonrec.status == "done") {
+							// Set innerHTML of currentlocation output box
+							document.querySelector('#currentlocation').value = jsonrec.payload.currentLocation;
+							
+							// Loop if destination != current location
+							if(jsonrec.payload.currentLocation == jsonrec.payload.destination) {
+								;
+							}
+							else {
+								fetchNow();
+							}
 						}
 					})
 					.catch((error) => {console.error('Error: ', error);});
-			};
+			});
         
         // RESET ALL FIELDS, DELETE ASSOCIATED CLIENT
 			document.querySelector('#source').disabled = false;
@@ -154,7 +155,22 @@ $(document).ready(function(){
 			document.querySelector('#robotid').value = "";
 			document.querySelector('#display').value = "";
 			
+			var jsondel = JSON.stringify({ 
+				"status": "good",
+				"clientID": clientID,
+				"opperation": "delete",
+				"payload": {
+					"sender": "",
+					"receiver": "",
+					"pickup": "",
+					"destination": ""}});
+			request = new Request('http://localhost:5000', {
+				method: 'POST',
+				mode: 'no-cors',
+				headers: {'Content-Type': 'application/json'},
+				body: jsondel});	
 			
-			// Todo delete client
+			response = await fetch(request)
+				.catch((error) => {console.error('Error: ', error);});
     });
 });
