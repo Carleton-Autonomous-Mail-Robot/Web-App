1b49206de8100cfc560ee18de3cde16fdd534fee
Devon Daley <devondaley@cmail.carleton.ca>
2021-03-15 23:41:17 -0400

    Made app current with rest of project, including API change. currLocation reading disabled

--- MainPage.js
+++ MainPage.js
@@ -13,28 +13,21 @@ $(document).ready(function(){
         // FETCH FORM INFO AND RESET FIELDS
 			let source = document.querySelector('#source').value;
 			let dest = document.querySelector('#destination').value;
-			document.querySelector('#display').value = "";
-			document.querySelector('#clientid').value = "";
-			document.querySelector('#currentlocation').value = "";
+			$('#display').val("");
+			$('#clientid').val("");
+			$('#currentlocation').val("");
 			document.querySelector('#display').style.backgroundColor = "white";
 			
 		// ENSURE THAT DEST != SRC
 		if (dest == source) {
-			document.querySelector('#display').value = "Invaid because destination = source";
+			$('#display').val("Invaid because destination = source");
 			return "";
-		}
+		};
         
-        // CREATE NEW CLIENT ON WEBSERVER, RESPONSE HAS ID INFO
-			var jsonnew = JSON.stringify({ 
-				"status": "good",
-				"clientID": "",
-				"opperation": "newClient",
-				"payload": "user"});
-			
-			let request = new Request('http://localhost:5000', {
-				method: 'POST',
-				headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
-				body: jsonnew});
+        // CREATE NEW CLIENT ON WEBSERVER, RESPONSE HAS ID INFO			
+			let request = new Request('http://localhost:8000/newClient?robot=yes', {
+				method: 'GET',
+				headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true}});
 				
 			// Send message, await response. Store IDs
 			let clientID = 0;
@@ -48,9 +41,13 @@ $(document).ready(function(){
 			if (success) {
 				if (jsonrec.status == "done") {
 					clientID = jsonrec.clientID;
+					$('#clientid').val(clientID);
+				} else {
+					$('#display').val("Server returned status = bad");
+					return ""
 				}
 			} else {
-				document.querySelector('#display').value = "Connection failed, server not found";
+				$('#display').val("Connection failed, server not found");
 				return "";
 			}
 			
@@ -58,32 +55,27 @@ $(document).ready(function(){
 			
         // IF YOU FOUND SERVER AND GOT VALID RESPONSE, MAKE OFFICIAL REQUEST
 			// Insert received data into json, make string for transit.
-			var jsonLmsg = JSON.stringify({ 
-				"status": "good",
-				"clientID": clientID,
-				"opperation": "leaveMessage",
-				"payload": {
-					"pickup": source,
-					"destination": dest,
-					"currentLocation": ""
-					}});
-				
-			request = new Request('http://localhost:5000', {
+			var url = 'http://localhost:8000/rawLeaveMessage?clientID=' + clientID.toString();
+			let pay = source + dest
+			
+			request = new Request(url, {
 				method: 'POST',
 				headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
-				body: jsonLmsg});
+				body: JSON.stringify({'payload': pay})});
 				
 			jsonrec = await fetch(request)
 				.then(response => response.json())
 				.catch((error) => {console.error('Error: ', error);success = false;});
-				
+			
+			console.log(jsonrec);
+			
 			if (success == false) {
-				document.querySelector('#display').value = "Server didn't respond";
+				$('#display').val("Server didn't respond");
 				return "";
 			}
 			if (jsonrec.status == 'bad') {
-				document.querySelector('#display').value = "No robots to service";
-				return ""
+				$('#display').val("No robots to service");
+				return "";
 			}
 			
         
@@ -93,140 +85,149 @@ $(document).ready(function(){
 			document.querySelector('#requestButton').disabled = true;
 			
         // LOOP UNTIL currentLocation == destination
-			var jsonloop = JSON.stringify({ 
-				"status": "good",
-				"clientID": clientID,
-				"opperation": "getMessage",
-				"payload": ""});
+			//var jsonloop = JSON.stringify({ 
+				//"status": "good",
+				//"clientID": clientID,
+				//"opperation": "getMessage",
+				//"payload": ""});
 			
-			request = new Request('http://localhost:5000', {
-				method: 'POST',
-				headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
-				body: jsonloop});
+			//request = new Request('http://localhost:8000', {
+				//method: 'POST',
+				//headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
+				//body: jsonloop});
 				
-			// Update display with the robotID
-			document.querySelector('#clientid').value = clientID;
+			//// Update display with the robotID
+			//document.querySelector('#clientid').value = clientID;
 			
-			// Loop until robot has reached destination
-			timeout = 0;
-			var fetchNow = await new Promise(resolve => async function() {
-				if (timeout < 60){
-					if (timeout == 30) {
-						document.querySelector('#display').style.backgroundColor = "yellow";
-					} else if (timeout == 45) {
-						document.querySelector('#display').style.backgroundColor = "orange";
-					} else if (timeout == 55) {
-						document.querySelector('#display').style.backgroundColor = "red";
-					}
-					await sleep(1000);		// Get updates once per second to not overload system
-					fetch(request)
-						.then(function(response) {
-							// Unpack payload from inside JSON
-							jsonrec = response.json();
+			//// Loop until robot has reached destination
+			//timeout = 0;
+			//var fetchNow = await new Promise(resolve => async function() {
+				//if (timeout < 60){
+					//if (timeout == 30) {
+						//document.querySelector('#display').style.backgroundColor = "yellow";
+					//} else if (timeout == 45) {
+						//document.querySelector('#display').style.backgroundColor = "orange";
+					//} else if (timeout == 55) {
+						//document.querySelector('#display').style.backgroundColor = "red";
+					//}
+					//await sleep(1000);		// Get updates once per second to not overload system
+					//fetch(request)
+						//.then(function(response) {
+							//// Unpack payload from inside JSON
+							//jsonrec = response.json();
 							
-							// Check if the getmessage succeeded
-							if (jsonrec.status == "done") {
-								// reset timeout
-								timeout = 0;
-								document.querySelector('#display').style.backgroundColor = "white";
+							//// Check if the getmessage succeeded
+							//if (jsonrec.status == "done") {
+								//// reset timeout
+								//timeout = 0;
+								//document.querySelector('#display').style.backgroundColor = "white";
 								
-								// Set innerHTML of currentlocation output box
-								document.querySelector('#currentlocation').value = jsonrec.payload.currentLocation;
+								//// Set innerHTML of currentlocation output box
+								//document.querySelector('#currentlocation').value = jsonrec.payload.currentLocation;
 								
-								// Loop if destination != current location
-								if(jsonrec.payload.currentLocation == jsonrec.payload.destination) {;}
-								else {
-									fetchNow();
-								}
-							} else {
-								fetchNow();
-								timeout = timeout + 1;
-							}
-						})
-						.catch((error) => {
-							console.error('Error: ', error);
-							timeout = timeout + 1;
-							fetchNow();
-						});
-				}
-			});
+								//// Loop if destination != current location
+								//if(jsonrec.payload.currentLocation == jsonrec.payload.destination) {;}
+								//else {
+									//fetchNow();
+								//}
+							//} else {
+								//fetchNow();
+								//timeout = timeout + 1;
+							//}
+						//})
+						//.catch((error) => {
+							//console.error('Error: ', error);
+							//timeout = timeout + 1;
+							//fetchNow();
+						//});
+				//}
+			//});
 			
-			if (timeout == 60) {
-				document.querySelector('#display').value = "TIMEOUT: Too long since robot sent location, robot is lost";
-			} else {
-				document.querySelector('#display').value = "";
-			}
+			//if (timeout == 60) {
+				//document.querySelector('#display').value = "TIMEOUT: Too long since robot sent location, robot is lost";
+			//} else {
+				//document.querySelector('#display').value = "";
+			//}
         
         // RESET ALL FIELDS, DELETE ASSOCIATED CLIENT
 			document.querySelector('#source').disabled = false;
 			document.querySelector('#destination').disabled = false;
 			document.querySelector('#requestButton').disabled = false;
 			
-			document.querySelector('#currentlocation').value = "";
-			document.querySelector('#clientid').value = "";
+			//document.querySelector('#currentlocation').val("");
+			//document.querySelector('#clientid').val("");
 			
-			var jsondel = JSON.stringify({ 
-				"status": "good",
-				"clientID": clientID,
-				"opperation": "delete",
-				"payload": ""});
+			//var jsondel = JSON.stringify({ 
+				//"status": "good",
+				//"clientID": clientID,
+				//"opperation": "delete",
+				//"payload": ""});
 				
-			request = new Request('http://localhost:5000', {
-			method: 'POST',
-			headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
-			body: jsondel});
+			//request = new Request('http://localhost:8000', {
+			//method: 'POST',
+			//headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
+			//body: jsondel});
 			
-			jsonrec = await fetch(request)
-				.catch((error) => {console.error('Error: ', error);});
+			//jsonrec = await fetch(request)
+				//.catch((error) => {console.error('Error: ', error);});
     });
     
     // send test message
     $("#testButton").click(async function () {
 		// Get information to be sent
-		let status = document.querySelector('#status').value;
 		let sentID = document.querySelector('#sentID').value;
 		let opperation = document.querySelector('#opperation').value;
 		
-		// Get payload, either basic or other
-		if ($("#paytype").is(':checked')) {
-			let payload = document.querySelector('#payload').value
-		} else {
-			let pickup = document.querySelector('#pickup').value;
-			let dropoff = document.querySelector('#dropoff').value;
-			let location = document.querySelector('#location').value;
-			let payload = {"pickup":pickup,"destination":dropoff,"currentLocation":location}
-		}
+		// Get payload, info
+		let payload = document.querySelector('#payload').value;
+		let pickup = document.querySelector('#pickup').value;
+		let dropoff = document.querySelector('#dropoff').value;
 		
-		// Make message and request
-		var json = JSON.stringify({ 
-				"status": status,
-				"clientID": sentID,
-				"opperation": opperation,
-				"payload": payload});
+		// set value depending on opperation
+		url = 'http://localhost:8000/';
+		meth = '';
+		pay = '';
+		head = {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true};
+		
+		
+		if (opperation == 'leaveMessage'){
+			url += 'rawLeaveMessage?clientID=' + sentID;
+			meth = 'POST';
+			pay = {'payload':payload};
 			
-		let request = new Request('http://localhost:5000', {
-			method: 'POST',
-			headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true},
-			body: json});
+			// Make Request
+			request = new Request(url, {
+			method: meth,
+			headers: head,
+			body: pay});
+		}
+		else {
+			if (opperation == 'delete'){
+				url += 'delete?clientID=' + sentID;
+				meth = 'GET';}
+			else if (opperation == 'getMessage'){
+				url += 'getMessage?clientID=' + sentID;
+				meth = 'GET';}
+			else if (opperation == 'newClient'){
+				url += 'newClient';
+				meth = 'GET';}
+			
+			// Make Request
+			request = new Request(url, {
+			method: meth,
+			headers: head});
+		}
+			
 		
 		// Send message
+		success = true;
 		let jsonrec = await fetch(request)
 			.then(response => response.json())
 			.catch((error) => {console.error('Error: ', error);success = false;});
 			
-		// Log msg and response
-		$("#output").value += "\n" + json;
-		$("#output").value += "\n" + jsonrec + "\n";
-	});
-	
-	// change type of message you're trying to send
-	$("#paytype").click(function () {
-		if($(this).is(':checked')){
-			$("#basicPayload").show();
-			$("#otherPayload").hide();
-		} else {
-			$("#basicPayload").hide();
-			$("#otherPayload").show();
+		// Log response
+		if (success) {
+			$("#output").val(JSON.stringify(jsonrec));
 		}
 	});
 	
